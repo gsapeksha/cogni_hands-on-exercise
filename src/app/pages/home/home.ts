0ba2a96025251dae9950a633053caf2bf1102137
Apeksha GS <gsapeksha@gmail.com>
2026-06-24 09:14:41 +0530

    Hands-On 2 - Implemented data binding

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+  studentName = 'Apeksha';
+
+  course = 'Angular';
+
+  isDisabled = false;
+
+  count = 0;
+
+  increaseCount() {
+    this.count++;
+  }
+
+}
